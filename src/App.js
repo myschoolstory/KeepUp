e2ea4a0f8388fc07ef3cad009f533451a8a33c87
myschoolstory <168579347+myschoolstory@users.noreply.github.com>
2024-09-04 11:56:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ export default function App() {
         }));
         setNews(newsData);
       } catch (error) {
-        console.error('Error fetching the news:', error);
+        console.error('Error fetching the news...:', error);
       }
     };
 
